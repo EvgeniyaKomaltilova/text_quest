620377951a932ca8572ba75ff2a00030a35b5c79
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-04-18 11:40:47 +0700

    + random message

--- src/Game.java
+++ src/Game.java
@@ -53,10 +53,22 @@ public class Game {
             if (key.equals(entry.getKey())) {
                 currentLocation = entry.getValue();
                 Location current = entry.getValue();
-                System.out.println("Вы находитесь " + current.name);
+                System.out.print("Вы находитесь " + current.name);
+                if (!current.isOpen) {
+                    if (current.firstMessage!=null) {
+                        System.out.println(current.firstMessage);
+                        current.isOpen = true;
+                    }
+                } else {
+                    if (current.messages.size() != 0) {
+                        double x = Math.random()*current.messages.size();
+                        System.out.println(current.messages.get((int) x));
+                    }
+                }
                 System.out.println("Вы можете:");
-                for (String s : current.actions) {
-                    System.out.println("* " + s);
+                for (Map.Entry<String, Boolean> locEntry : current.actions.entrySet()) {
+                    if (locEntry.getValue())
+                    System.out.println("* " + locEntry.getKey());
                 }
                 for (Map.Entry<String, Boolean> currentEntry : current.ways.entrySet()) {
                     if (currentEntry.getValue()) {
@@ -77,19 +89,31 @@ public class Game {
 
         bedRoom.ways.put("Идти в коридор", true);
         bedRoom.items.add("ключ");
-        bedRoom.actions.add("Обыскать комнату");
+        bedRoom.actions.put("Обыскать комнату", true);
 
         hallway.ways.put("Идти в гостиную", true);
         hallway.ways.put("Идти в спальню", true);
         hallway.ways.put("Выйти наружу!", false);
-        hallway.actions.add("Открыть дверь");
+        hallway.actions.put("Открыть дверь", false);
 
         livingRoom.ways.put("Идти на кухню", true);
         livingRoom.ways.put("Идти в коридор", true);
+        livingRoom.actions.put("Обыскать комнату", true);
 
         kitchen.ways.put("Идти в ванную", true);
         kitchen.ways.put("Идти в гостиную", true);
+        kitchen.actions.put("Обыскать комнату", true);
+        kitchen.firstMessage = " Испуганные тараканы бросились врассыпную.";
+        kitchen.messages.add(" С потолка свисает длинная нить паутины.");
+        kitchen.messages.add(" Сквозь пыльное окно едва пробивается лучик света.");
+        kitchen.messages.add(" Цветы на подоконнике, очевидно, давно никто не поливал.");
+        kitchen.messages.add(" С потолка свисает длинная нить паутины.");
+        kitchen.messages.add(" За окном слышно пение птиц.");
+        kitchen.messages.add(" Через щели в полу дует ветер.");
 
         bathRoom.ways.put("Идти на кухню", true);
+        bathRoom.items.add("зубную щетку");
+        bathRoom.actions.put("Обыскать комнату", true);
+        bathRoom.actions.put("Почистить зубы", false);
     }
 }
--- src/Location.java
+++ src/Location.java
@@ -3,20 +3,33 @@ import java.util.*;
 public class Location {
     String name;
     List<String> items;
-    List<String> actions;
+    Map<String, Boolean> actions;
     Map<String, Boolean> ways;
+    boolean isOpen;
+    String firstMessage;
+    List<String> messages;
 
     public Location(String name) {
         this.name = name;
         this.ways = new HashMap<>();
-        this.actions = new ArrayList<>();
+        this.actions = new HashMap<>();
         this.items = new ArrayList<>();
+        this.messages = new ArrayList<>();
     }
 
     public void searchItem() {
         if (this.items.size() != 0) {
-            Game.inventory.add(this.items.get(0));
-            System.out.println("Вы нашли " + this.items.get(0) + "!");
+            String item = this.items.get(0);
+            Game.inventory.add(item);
+            System.out.println("Вы нашли " + item + "!");
+            switch (item) {
+                case "ключ" :
+                    Game.hallway.actions.replace("Открыть дверь", false, true);
+                    break;
+                case "зубную щетку" :
+                    Game.bathRoom.actions.replace("Почистить зубы", false, true);
+                    break;
+            }
             this.items.remove(0);
         } else
             System.out.println("Вы ничего не нашли...");
